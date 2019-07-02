e08285074985c2d411bf88be4ad8579f8f7d84b8
alok <alokdwivedi16@gmail.com>
2019-07-02 10:48:46 +0530

    first

---
+++ src/test/java/TestMultiplication.java
@@ -0,0 +1,19 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class TestMultiplication {
+
+
+	@Test
+	public void testAdd() {
+		//WebDriverManager.chromedriver().setup();
+		//WebDriver driver = new ChromeDriver();
+		//driver.get("https//facebook.com");
+		System.out.println("hi");
+
+	}
+
+}
